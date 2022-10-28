b21980bf69a7edd30a703c62a531ac8ba43309f3
Paulo Ferreira <phgferreira@outlook.com>
2022-10-27 23:24:17 -0300

    Uma outra forma de fazer a mudança de atributo mas dessa vez antes de chegar no Servlet é usando o Model no lugar do HttpServletRequest

--- src/main/java/br/com/alura/mvc/aluramvc/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/aluramvc/controller/HelloController.java
@@ -1,6 +1,7 @@
 package br.com.alura.mvc.aluramvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,9 +10,17 @@ import javax.servlet.http.HttpServletRequest;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String helloWorld(HttpServletRequest request) {
-        request.setAttribute("nome", "Pedro Pereira");
+    // Uma outra forma de adicionar esse atributo é usando o Model
+    public String helloWorld(Model model) {
+        model.addAttribute("nome", "Pedro Pereira");
         // Aqui nós passamos o nome da view que é o hello.hmlt (não precisa colocar o .html)
         return "hello";
     }
+
+//    @GetMapping("/hello")
+//    public String helloWorld(HttpServletRequest request) {
+//        request.setAttribute("nome", "Pedro Pereira");
+//        // Aqui nós passamos o nome da view que é o hello.hmlt (não precisa colocar o .html)
+//        return "hello";
+//    }
 }
