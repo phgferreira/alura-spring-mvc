ca9b5c1964084e1f31447728d2cc0c9bfbb213b2
Paulo Ferreira <phgferreira@outlook.com>
2022-10-27 22:34:18 -0300

    Iniciado com Spring Boot

---
+++ src/main/java/br/com/alura/mvc/aluramvc/AluramvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc.aluramvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluramvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluramvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/mvc/aluramvc/AluramvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.mvc.aluramvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluramvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
