e5f8b3bce0cdba5e27dfa6b18b6a3d8d632a0d61
Paulo Ferreira <phgferreira@outlook.com>
2022-10-27 23:20:50 -0300

    Colocando um atributo dentro do HttpServletRequest para inserir na página

--- src/main/java/br/com/alura/mvc/aluramvc/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/aluramvc/controller/HelloController.java
@@ -3,11 +3,14 @@ package br.com.alura.mvc.aluramvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String helloWorld() {
+    public String helloWorld(HttpServletRequest request) {
+        request.setAttribute("nome", "Pedro Pereira");
         // Aqui nós passamos o nome da view que é o hello.hmlt (não precisa colocar o .html)
         return "hello";
     }
