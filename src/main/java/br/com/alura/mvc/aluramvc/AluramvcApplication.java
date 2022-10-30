2270e3116c28dfc757902784a171900386a1d591
Paulo Ferreira <phgferreira@outlook.com>
2022-10-30 08:04:25 -0300

    Corrigido para não salvar vários pedido iguais logo no início da aplicação

--- src/main/java/br/com/alura/mvc/aluramvc/AluramvcApplication.java
+++ src/main/java/br/com/alura/mvc/aluramvc/AluramvcApplication.java
@@ -24,17 +24,22 @@ public class AluramvcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Smart TV LG 55UQ801C0SB LCD 4K 55\" 100V/240V");
-		pedido.setValorNegociado( new BigDecimal("3099.00"));
-		pedido.setDataEntrega(LocalDate.parse("2022-12-30") );
-		pedido.setUrlProduto("https://www.mercadolivre.com.br/smart-tv-lg-55uq801c0sb-lcd-4k-55-100v240v/p/MLB19641718?pdp_filters=item_id:MLB2900977372#searchVariation=MLB19641718&position=1&search_layout=grid&type=pad&tracking_id=7037e6a8-b613-41b5-90e5-e045f7f8a435&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=ZmVlZjJhNzItOGJiMC00NTQ0LWJiNDAtNGY5ZjU3OGE5Njlh");
-		pedido.setUrlImagem("https://http2.mlstatic.com/D_NQ_NP_680264-MLA51736863750_092022-O.webp");
-		pedido.setDescricao("""
+		// Produto de teste não existir no banco ainda, então insira-o
+		if (pedidoRepository.findByNomeProduto("Smart TV LG 55UQ801C0SB LCD 4K 55\" 100V/240V").size() == 0) {
+			Pedido pedido = new Pedido();
+			pedido.setNomeProduto("Smart TV LG 55UQ801C0SB LCD 4K 55\" 100V/240V");
+			pedido.setValorNegociado( new BigDecimal("3099.00"));
+			pedido.setDataEntrega(LocalDate.parse("2022-12-30") );
+			pedido.setUrlProduto("https://www.mercadolivre.com.br/smart-tv-lg-55uq801c0sb-lcd-4k-55-100v240v/p/MLB19641718?pdp_filters=item_id:MLB2900977372#searchVariation=MLB19641718&position=1&search_layout=grid&type=pad&tracking_id=7037e6a8-b613-41b5-90e5-e045f7f8a435&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=ZmVlZjJhNzItOGJiMC00NTQ0LWJiNDAtNGY5ZjU3OGE5Njlh");
+			pedido.setUrlImagem("https://http2.mlstatic.com/D_NQ_NP_680264-MLA51736863750_092022-O.webp");
+			pedido.setDescricao("""
                 A LG é inovação e isso pode ser visto em cada um dos seus produtos tecnológicos, especialmente pensados \u200B\u200Bpara que toda a família aproveite mais a vida. Ter uma televisão LG é tirar proveito da mais alta qualidade do mercado.
                 Com Smart TV 55UQ801 você poderá acessar os aplicativos onde seu conteúdo favorito está localizado. Além disso, você pode navegar na web, interagir em redes sociais e se divertir com jogos de vídeo."""
-		);
+			);
+			pedidoRepository.save(pedido);
+		}
 
-		pedidoRepository.save(pedido);
+
+
 	}
 }
--- src/main/java/br/com/alura/mvc/aluramvc/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/aluramvc/repository/PedidoRepository.java
@@ -3,5 +3,10 @@ package br.com.alura.mvc.aluramvc.repository;
 import br.com.alura.mvc.aluramvc.model.Pedido;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PedidoRepository extends CrudRepository<Pedido, Integer> {
+
+    List<Pedido> findByNomeProduto(String nomeProduto);
+
 }
