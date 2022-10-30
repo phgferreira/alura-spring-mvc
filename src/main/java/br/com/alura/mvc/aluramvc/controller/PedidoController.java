ae6121305798af0d35ff617036c54b267c8e3abc
Paulo Ferreira <phgferreira@outlook.com>
2022-10-30 10:09:09 -0300

    Salvando um pedido com sucesso

--- src/main/java/br/com/alura/mvc/aluramvc/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/aluramvc/controller/PedidoController.java
@@ -1,16 +1,32 @@
 package br.com.alura.mvc.aluramvc.controller;
 
+import br.com.alura.mvc.aluramvc.dto.PedidoDto;
+import br.com.alura.mvc.aluramvc.model.Pedido;
+import br.com.alura.mvc.aluramvc.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pedido")
+@RequestMapping("pedido")
 public class PedidoController {
 
-    @GetMapping("/formulario")
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping("formulario")
     public String formulario() {
         return "pedido/formulario";
+    }
+
+    @PostMapping("novo")
+    public String novo(PedidoDto pedidoDto) {
+        Pedido pedido = pedidoDto.toPedido();
+        pedidoRepository.save(pedido);
+
+        return "home";
     }
 
 }
---
+++ src/main/java/br/com/alura/mvc/aluramvc/dto/PedidoDto.java
@@ -0,0 +1,62 @@
+package br.com.alura.mvc.aluramvc.dto;
+
+import br.com.alura.mvc.aluramvc.model.Pedido;
+
+public class PedidoDto {
+
+    private String nomeProduto;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    @Override
+    public String toString() {
+        return "RequisicaoNovoPedido{" +
+                "nomeProduto='" + nomeProduto + '\'' +
+                ", urlProduto='" + urlProduto + '\'' +
+                ", urlImagem='" + urlImagem + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Pedido toPedido() {
+        Pedido pedido = new Pedido();
+        pedido.setNomeProduto(this.nomeProduto);
+        pedido.setUrlProduto(this.urlProduto);
+        pedido.setUrlImagem(this.urlImagem);
+        pedido.setDescricao(this.descricao);
+        return pedido;
+    }
+}
