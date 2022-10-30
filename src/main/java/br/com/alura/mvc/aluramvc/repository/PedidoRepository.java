112ae364c7ac2f4119ae887f6c3e444e4bdd1aef
Paulo Ferreira <phgferreira@outlook.com>
2022-10-30 08:28:44 -0300

    Inserido um simples comentário no PedidoRepository

--- src/main/java/br/com/alura/mvc/aluramvc/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/aluramvc/repository/PedidoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/* Usando JPA precisa usar a anotação @Repository mas extendendos a CrudRepository não precisa,
+*  eu imagino que o motivo seja a anotação @NoRepositoryBean do CrudRepository */
 public interface PedidoRepository extends CrudRepository<Pedido, Integer> {
 
     List<Pedido> findByNomeProduto(String nomeProduto);
