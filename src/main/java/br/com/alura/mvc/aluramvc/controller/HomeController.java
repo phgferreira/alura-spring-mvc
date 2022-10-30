0de5289fdc2d24192acb11a27d2e01bd52e7947d
Paulo Ferreira <phgferreira@outlook.com>
2022-10-30 08:22:23 -0300

    Usando PedidoRepository no HomeController

--- src/main/java/br/com/alura/mvc/aluramvc/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/aluramvc/controller/HomeController.java
@@ -1,6 +1,8 @@
 package br.com.alura.mvc.aluramvc.controller;
 
 import br.com.alura.mvc.aluramvc.model.Pedido;
+import br.com.alura.mvc.aluramvc.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,23 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.math.BigDecimal;
-import java.sql.Statement;
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
     @GetMapping("/home")
     public String home(Model model) {
-
-        Query query = entityManager.createQuery("select p from Pedido p");
-        List<Pedido> pedidos = query.getResultList();
+        Iterable<Pedido> pedidos = pedidoRepository.findAll();
 
         model.addAttribute("pedidos", pedidos );
         return "home";
